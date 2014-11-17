0db12c79e3496b20398246c956241b8d2bbc1519
Aleš Komárek <mail@newt.cz>
2014-11-17 15:16:33 +0100

    reverse logic

--- relay/sensor.py
+++ relay/sensor.py
@@ -9,6 +9,13 @@ def get_data(sensor):
     f = open(status_file, 'r')
     value = int(f.read())
     f.close()
+
+  if sensor.get('reverse', False):
+    if value == 0:
+      value = 1
+    if value == 1:
+      value = 0
+
   except Exception, e:
     raise e
     value = 0
